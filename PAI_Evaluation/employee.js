717faca5aee981e07bb45d63a8cb2c1039111e80
Debashrita Sahoo <138135798+debashritaSahoo12@users.noreply.github.com>
2025-04-28 11:31:34 +0530

    Update employee.js

--- PAI_Evaluation/employee.js
+++ PAI_Evaluation/employee.js
@@ -1,11 +1,10 @@
-function Employee(name, position, salary) {
+function Employee(name,position,salary) {
   this.name = name;
   this.salary = salary;
   this.position = position;
-  this.getDetails = function () {
-    return `${name} works as a ${position} and earns ${salary}.`;
-  };
 }
-
+Employee.prototype.getDetails=function(){
+     return `${this.name} works as a ${this.position} and earns ${this.salary}.`;
+}
 const employee1 = new Employee("Alice", "Developer", 80000);
 console.log(employee1.getDetails());
